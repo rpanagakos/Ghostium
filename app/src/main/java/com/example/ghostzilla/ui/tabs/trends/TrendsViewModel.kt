45c1176d07a26e5caba80ce7431bfab399e27164
romanos <romanos.panagakos@gmail.com>
2021-11-13 23:44:09 +0200

    Implementation: Testing classes

--- app/src/main/java/com/example/ghostzilla/ui/tabs/trends/TrendsViewModel.kt
+++ app/src/main/java/com/example/ghostzilla/ui/tabs/trends/TrendsViewModel.kt
@@ -1,16 +1,14 @@
 package com.example.ghostzilla.ui.tabs.trends
 
-import androidx.annotation.VisibleForTesting
 import androidx.lifecycle.viewModelScope
 import com.example.ghostzilla.abstraction.AbstractViewModel
 import com.example.ghostzilla.models.coingecko.Markets
-import com.example.ghostzilla.models.coingecko.MarketsItem
 import com.example.ghostzilla.models.generic.GenericResponse
 import com.example.ghostzilla.network.DataRepository
 import com.example.ghostzilla.utils.SingleLiveEvent
+import com.example.ghostzilla.utils.wrapEspressoIdlingResource
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.isActive
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -19,16 +17,24 @@ class TrendsViewModel @Inject constructor(
     private val dataRepository: DataRepository
 ) : AbstractViewModel() {
 
-    val marketsLiveData  = SingleLiveEvent<Markets>()
+    val marketsLiveData = SingleLiveEvent<Markets>()
 
-    fun getMarkets(){
+    fun getMarkets() {
         viewModelScope.launch {
-            dataRepository.requestData().collect {
-                when(it){
-                    is GenericResponse.Success -> it.data?.let {  marketsLiveData.value = it } ?: run { showToastMessage(0)  }
-                    is GenericResponse.DataError -> it.errorCode?.let {error -> showToastMessage(error)  }
+            wrapEspressoIdlingResource {
+                dataRepository.requestData().collect {
+                    when (it) {
+                        is GenericResponse.Success -> it.data?.let { marketsLiveData.value = it }
+                            ?: run { showToastMessage(0) }
+                        is GenericResponse.DataError -> it.errorCode?.let { error ->
+                            showToastMessage(
+                                error
+                            )
+                        }
+                    }
                 }
             }
+
         }
     }
 
