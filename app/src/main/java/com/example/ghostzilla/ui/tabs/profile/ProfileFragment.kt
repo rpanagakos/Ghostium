39c19c722ab0b8f746819dd6ace5f648d3d043d1
romanos <romanos.panagakos@gmail.com>
2022-01-19 00:43:19 +0200

    Small sample of datastore

--- app/src/main/java/com/example/ghostzilla/ui/tabs/profile/ProfileFragment.kt
+++ app/src/main/java/com/example/ghostzilla/ui/tabs/profile/ProfileFragment.kt
@@ -1,10 +1,16 @@
 package com.example.ghostzilla.ui.tabs.profile
 
+import android.widget.Toast
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
 import com.example.ghostzilla.R
 import com.example.ghostzilla.abstraction.AbstractFragment
+import com.example.ghostzilla.database.security.DataStoreUtil
 import com.example.ghostzilla.databinding.FragmentProfileBinding
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class ProfileFragment :
@@ -12,8 +18,16 @@ class ProfileFragment :
 
     override val viewModel: ProfileViewModel by viewModels()
 
+    @Inject
+    lateinit var dataStore: DataStoreUtil
+
     override fun initLayout() {
         viewModel.runOperation()
+        lifecycleScope.launch {
+            dataStore.getUserInfo("2").collect {
+                Toast.makeText(requireContext(), it.toString(), Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 
     override fun observeViewModel() {
