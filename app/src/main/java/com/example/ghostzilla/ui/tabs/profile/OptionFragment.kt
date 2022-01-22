fe69ab9e0b7d7374ff161d1ce4f37db2e3b2389c
romanos <romanos.panagakos@gmail.com>
2022-01-22 21:21:07 +0200

    Add: Options Fragment

---
+++ app/src/main/java/com/example/ghostzilla/ui/tabs/profile/OptionFragment.kt
@@ -0,0 +1,30 @@
+package com.example.ghostzilla.ui.tabs.profile
+
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.navArgs
+import com.example.ghostzilla.R
+import com.example.ghostzilla.abstraction.AbstractFragment
+import com.example.ghostzilla.databinding.FragmentOptionBinding
+import com.example.ghostzilla.ui.tabs.trends.TrendsViewModel
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class OptionFragment :
+    AbstractFragment<FragmentOptionBinding, TrendsViewModel>(R.layout.fragment_option) {
+
+    private val args: OptionFragmentArgs by navArgs()
+
+    override val viewModel: TrendsViewModel by viewModels()
+
+
+    override fun initLayout() {
+        binding.title = args.title
+    }
+
+    override fun observeViewModel() {
+    }
+
+    override fun stopOperations() {
+    }
+
+}
