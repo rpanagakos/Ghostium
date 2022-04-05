4875be29672c55dd1f6e22788015e7fb35fcb528
romanos <romanos.panagakos@gmail.com>
2022-04-05 20:30:02 +0300

    Add: ArticleDetails Activity

---
+++ app/src/main/java/com/example/ghostzilla/ui/tabs/articles/ArticleDetailsActivity.kt
@@ -0,0 +1,18 @@
+package com.example.ghostzilla.ui.tabs.articles
+
+import com.example.ghostzilla.R
+import com.example.ghostzilla.abstraction.AbstractActivity
+import com.example.ghostzilla.databinding.ActivityArticleDetailsBinding
+
+class ArticleDetailsActivity :
+    AbstractActivity<ActivityArticleDetailsBinding>(R.layout.activity_article_details) {
+
+    override fun initLayout() {
+    }
+
+    override fun runOperation() {
+    }
+
+    override fun stopOperation() {
+    }
+}
