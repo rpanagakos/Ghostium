538bfcb0e8f303408da0d0ac237b8b491a755d89
romanos <romanos.panagakos@gmail.com>
2022-04-06 22:44:45 +0300

    Add: Recyclerview and backToTop

--- app/src/main/java/com/example/ghostzilla/ui/tabs/settings/bookmark/BookmarkFragment.kt
+++ app/src/main/java/com/example/ghostzilla/ui/tabs/settings/bookmark/BookmarkFragment.kt
@@ -1,10 +1,14 @@
 package com.example.ghostzilla.ui.tabs.settings.bookmark
 
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
 import com.example.ghostzilla.R
 import com.example.ghostzilla.abstraction.AbstractFragment
 import com.example.ghostzilla.databinding.FragmentBookmarkBinding
+import com.example.ghostzilla.ui.tabs.common.TabsActivity
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.fragment_favourite.*
 
 
 @AndroidEntryPoint
@@ -12,8 +16,19 @@ class BookmarkFragment :
     AbstractFragment<FragmentBookmarkBinding, BookmarkViewModel>(R.layout.fragment_bookmark) {
 
     override val viewModel: BookmarkViewModel by viewModels()
+    private val args: BookmarkFragmentArgs by navArgs()
 
-    override fun initLayout() {}
+
+    override fun initLayout() {
+        binding.title = args.title
+        onBackPressed {
+            (requireActivity() as TabsActivity).showMenuBar()
+        }
+        backButtonFavourite.setOnClickListener {
+            (requireActivity() as TabsActivity).showMenuBar()
+            findNavController().popBackStack()
+        }
+    }
 
     override fun observeViewModel() {}
 
