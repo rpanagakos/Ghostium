c60234a5fa564ea1e9fdd0898a4dc98faa653ab3
romanos <romanos.panagakos@gmail.com>
2022-04-22 01:59:24 +0300

    Add: Webview files

---
+++ app/src/main/java/com/rdp/ghostium/ui/tabs/common/WebviewActivity.kt
@@ -0,0 +1,18 @@
+package com.rdp.ghostium.ui.tabs.common
+
+import com.rdp.ghostium.R
+import com.rdp.ghostium.abstraction.AbstractActivity
+import com.rdp.ghostium.databinding.ActivityWebviewBinding
+
+class WebviewActivity : AbstractActivity<ActivityWebviewBinding>(R.layout.activity_webview) {
+
+    override fun initLayout() {
+    }
+
+    override fun runOperation() {
+    }
+
+    override fun stopOperation() {
+    }
+
+}
